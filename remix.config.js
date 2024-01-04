542a1db5d157971bb7d6999c36fc0cf98762e488
Peter Holló <hello@songsforthe.dev>
2024-01-04 22:23:32 +0100

    fix: vitest bundled to app and throws error

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@ import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ["**/.*", "**/*.{test,spec}.{js,ts,jsx,tsx}"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
